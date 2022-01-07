3d9c41e534d14561fc2548a0c00e717175f4fe7b
cmdc0de <cmdc0dez@gmail.com>
2022-01-06 22:30:44 -0700

    not compiling leds do work!

---
+++ firmware/main/app.cpp
@@ -0,0 +1,265 @@
+/*
+ * app.cpp
+ *
+ * Author: cmdc0de
+ */
+
+#include "app.h"
+#include <esp_log.h>
+#include <system.h>
+#include <spibus.h>
+
+#include <driver/uart.h>
+#include <driver/gpio.h>
+#include <device/display/frame_buffer.h>
+#include <device/display/display_device.h>
+#include <device/display/fonts.h>
+#include <device/display/gui.h>
+#include <device/touch/XPT2046.h>
+#include "menus/menu_state.h"
+#include "menus/calibration_menu.h"
+#include <app/display_message_state.h>
+#include "device/leds/apa102.h"
+#include "spibus.h"
+#include "freertos.h"
+#include "fatfsvfs.h"
+#include "pinconfig.h"
+
+using libesp::ErrorType;
+using libesp::System;
+using libesp::FreeRTOS;
+using libesp::RGBB;
+using libesp::APA102c;
+using libesp::SPIBus;
+using libesp::DisplayILI9341;
+using libesp::XPT2046;
+using libesp::GUI;
+using libesp::DisplayMessageState;
+using libesp::BaseMenu;
+
+
+const char *MyApp::LOGTAG = "AppTask";
+const char *MyApp::sYES = "Yes";
+const char *MyApp::sNO = "No";
+
+#define START_ROT libesp::DisplayILI9341::LANDSCAPE_TOP_LEFT
+static const uint16_t PARALLEL_LINES = 1;
+
+libesp::DisplayILI9341 Display(MyApp::DISPLAY_WIDTH,MyApp::DISPLAY_HEIGHT,START_ROT,
+	PIN_NUM_DISPLAY_BACKLIGHT, PIN_NUM_DISPLAY_RESET);
+
+static uint16_t BkBuffer[MyApp::FRAME_BUFFER_WIDTH*MyApp::FRAME_BUFFER_HEIGHT];
+static uint16_t *BackBuffer = &BkBuffer[0];
+
+uint16_t ParallelLinesBuffer[MyApp::DISPLAY_WIDTH*PARALLEL_LINES] = {0};
+
+libesp::ScalingBuffer FrameBuf(&Display, MyApp::FRAME_BUFFER_WIDTH, MyApp::FRAME_BUFFER_HEIGHT, uint8_t(16), MyApp::DISPLAY
+_WIDTH,MyApp::DISPLAY_HEIGHT, PARALLEL_LINES, (uint8_t*)&BackBuffer[0],(uint8_t*)&ParallelLinesBuffer[0]);
+
+static GUI MyGui(&Display);
+static XPT2046 TouchTask(PIN_NUM_TOUCH_IRQ,true);
+static CalibrationMenu MyCalibrationMenu("nvs");
+
+
+const char *MyErrorMap::toString(int32_t err) {
+	return "TODO";
+}
+
+MyApp MyApp::mSelf;
+
+MyApp &MyApp::get() {
+	return mSelf;
+}
+
+MyApp::MyApp() : AppErrors(), CurrentMode(ONE), LastTime(0) {
+	ErrorType::setAppDetail(&AppErrors);
+}
+
+MyApp::~MyApp() {
+
+}
+
+uint8_t *MyApp::getBackBuffer() {
+	return (uint8_t *)&BackBuffer[0];
+}
+
+uint32_t MyApp::getBackBufferSize() {
+	return MyApp::FRAME_BUFFER_WIDTH*MyApp::FRAME_BUFFER_HEIGHT*2;
+}
+  
+static RGBB leds[256];
+static size_t NumLEDs = sizeof(leds)/sizeof(leds[0]);
+libesp::APA102c LedControl;
+
+libesp::ErrorType MyApp::onInit() {
+	ErrorType et;
+	ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+
+  et = APA102c::initAPA102c(PIN_NUM_LEDS_MOSI, PIN_NUM_lEDS_CLK, VSPI_HOST, 1);
+  if(!et.ok()) {
+    return et;
+  } else {
+    ESP_LOGI(LOGTAG,"APA102c inited");
+  }
+
+	ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+
+	SPIBus *vbus = SPIBus::get(VSPI_HOST);
+  et = LedControl.initDevice(vbus);
+
+  et = MyCalibrationMenu.initNVS();
+	if(!et.ok()) {
+		ESP_LOGE(LOGTAG,"failed to init nvs for calibration");
+		return et;
+	}
+
+	et = XPT2046::initTouch(PIN_NUM_TOUCH_MISO, PIN_NUM_TOUCH_MOSI, PIN_NUM_TOUCH_CLK,HSPI_HOST, 1);
+	ESP_LOGI(LOGTAG,"After Touch and Calibration: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getM
+inimumFreeHeapSize());
+
+	if(!et.ok()) {
+		ESP_LOGE(LOGTAG,"failed to touch");
+		return et;
+	}
+
+	SPIBus *hbus = libesp::SPIBus::get(HSPI_HOST);
+	et = TouchTask.init(hbus,PIN_NUM_TOUCH_CS);
+
+	if(!et.ok()) {
+		ESP_LOGE(LOGTAG,"failed to touch SPI");
+		return et;
+	}
+
+	DisplayILI9341::initDisplay(PIN_NUM_DISPLAY_MISO, PIN_NUM_DISPLAY_MOSI,
+			PIN_NUM_DISPLAY_CLK, 2, PIN_NUM_DISPLAY_DATA_CMD, PIN_NUM_DISPLAY_RESET,
+			PIN_NUM_DISPLAY_BACKLIGHT, HSPI_HOST);
+
+  ESP_LOGI(LOGTAG,"After Display: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeap
+Size());
+
+
+	FrameBuf.createInitDevice(hbus,PIN_NUM_DISPLAY_CS,PIN_NUM_DISPLAY_DATA_CMD);
+	
+	ESP_LOGI(LOGTAG,"After FrameBuf: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHea
+pSize());
+
+	ESP_LOGI(LOGTAG,"start display init");
+	et=Display.init(libesp::DisplayILI9341::FORMAT_16_BIT, &Font_6x10, &FrameBuf);
+
+  if(et.ok()) {
+		ESP_LOGI(LOGTAG,"display init OK");
+		Display.fillScreen(libesp::RGBColor::BLACK);
+		Display.swap();
+		ESP_LOGI(LOGTAG,"fill black done");
+		Display.fillRec(0,0,FRAME_BUFFER_WIDTH/4,10,libesp::RGBColor::RED);
+		Display.swap();
+		vTaskDelay(500 / portTICK_RATE_MS);
+		Display.fillRec(0,15,FRAME_BUFFER_WIDTH/2,10,libesp::RGBColor::WHITE);
+		Display.swap();
+		vTaskDelay(500 / portTICK_RATE_MS);
+		Display.fillRec(0,30,FRAME_BUFFER_WIDTH,10,libesp::RGBColor::BLUE);
+		Display.swap();
+		vTaskDelay(500 / portTICK_RATE_MS);
+		Display.drawRec(0,60,FRAME_BUFFER_WIDTH/2,20, libesp::RGBColor::GREEN);
+		Display.drawString(15,110,"Color Validation.",libesp::RGBColor::RED);
+		Display.drawString(30,120,"UVLight Box",libesp::RGBColor::BLUE, libesp::RGBColor::WHITE,1,false);
+		Display.swap();
+
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		ESP_LOGI(LOGTAG,"After Display swap:Free: %u, Min %u",System::get().getFreeHeapSize(),System::get().getMini
+mumFreeHeapSize());
+
+	} else {
+		ESP_LOGE(LOGTAG,"failed display init");
+	}
+
+	TouchTask.start();
+	ESP_LOGI(LOGTAG,"After Task starts: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFree
+HeapSize());
+
+  if(et.ok()) {
+    for(int i=0;i<NumLEDs;++i) {
+      leds[i].setBlue(255);
+      leds[i].setBrightness(100);
+    }
+    LedControl.init(NumLEDs, &leds[0]);
+    LedControl.send();
+    ESP_LOGI(LOGTAG,"OnInit:After Send: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+  } else {
+    ESP_LOGI(LOGTAG,"failed initDevice");
+  }
+
+  if(MyCalibrationMenu.hasBeenCalibrated()) {
+		setCurrentMenu(getCalibrationMenu());
+	} else {
+		setCurrentMenu(getMenuState());
+	}
+
+	return et;
+}
+
+static bool Down = true;
+
+ErrorType MyApp::onRun() {
+#if 0
+		  return ErrorType();
+#else
+  ErrorType et;
+  uint32_t timeSinceLast = FreeRTOS::getTimeSinceStart()-LastTime;
+  if(timeSinceLast>=TIME_BETWEEN_PULSES) {
+    LastTime = FreeRTOS::getTimeSinceStart();
+    switch(CurrentMode) {
+    case ONE:
+      {
+        for(int i=0;i<NumLEDs;++i) {
+#if 0
+          int16_t n = leds[i].getBrightness();
+          if(Down) n-=10;
+          else n+=10;
+          if(n>=100) Down = true;
+          else if(0<=n) Down = false;
+
+          leds[i].setBrightness(uint8_t(n&0xFF));
+#else
+          leds[i].setBlue(0);
+          leds[i].setBrightness(50);
+          leds[i].setGreen(255);
+#endif
+        }
+
+        LedControl.init(NumLEDs, &leds[0]);
+        LedControl.send();
+        CurrentMode = TWO;
+      }
+      break;
+    case TWO:
+      {
+        for(int i=0;i<NumLEDs;++i) {
+          leds[i].setBrightness(10);
+          leds[i].setGreen(0);
+          leds[i].setRed(255);
+        }
+        LedControl.init(NumLEDs, &leds[0]);
+        LedControl.send();
+        CurrentMode = THREE;
+      }
+      break;
+    case THREE:
+      {
+        for(int i=0;i<NumLEDs;++i) {
+          leds[i].setBrightness(5);
+          leds[i].setGreen(0);
+          leds[i].setRed(0);
+          leds[i].setBlue(255);
+        }
+        LedControl.init(NumLEDs, &leds[0]);
+        LedControl.send();
+      }
+      break;
+    }
+  }
+	return et;
+#endif
+}
+
+
---
+++ firmware/main/app.h
@@ -0,0 +1,83 @@
+/*
+ * app.h
+ *
+ * Author: cmdc0de
+ */
+
+#ifndef LIGHTBOX_APP_H
+#define LIGHTBOX_APP_H
+
+#include <app/app.h>
+
+namespace libesp {
+class GUI;
+class DisplayDevice;
+class DisplayMessageState;
+class XPT2046;
+};
+
+enum ERRORS {
+	APP_OK = libesp::ErrorType::APP_OK
+	, OTA_INIT_FAIL = libesp::ErrorType::APP_BASE + 1
+	, BT_INIT_FAIL
+	, GAME_TASK_INIT_FAIL
+	, EXPLOIT_TASK_INIT_FAIL
+	, WIFI_TASK_INIT_FAIL
+	, BUTTON_INIT_FAIL
+	, TOP_BOARD_INIT_FAIL
+};
+
+class MyErrorMap : public libesp::IErrorDetail {
+public:
+	virtual const char *toString(int32_t err);
+	virtual ~MyErrorMap() {}
+};
+
+class MyApp : public libesp::App {
+public:
+  enum MODE {
+    ONE,
+    TWO
+    , THREE
+  };
+public:
+	static const char *LOGTAG;
+	//static const int QUEUE_SIZE = 10;
+	//static const int ITEM_SIZE = sizeof(MyAppMsg);
+	static const char *sYES;
+	static const char *sNO;
+  static const uint32_t TIME_BETWEEN_PULSES = 1000;
+  static const uint16_t DISPLAY_HEIGHT		= 240;
+	static const uint16_t DISPLAY_WIDTH			= 320;
+	//reminder ESP32 has 160KiB static and DRAM So a 1:1 buffer doesn't fit.
+	static const uint16_t FRAME_BUFFER_HEIGHT	= 144;
+	static const uint16_t FRAME_BUFFER_WIDTH	= 192;
+
+	static MyApp &get();
+public:
+	virtual ~MyApp();
+  uint16_t getCanvasWidth();
+	uint16_t getCanvasHeight();
+	uint16_t getLastCanvasWidthPixel();
+	uint16_t getLastCanvasHeightPixel();
+	libesp::DisplayDevice &getDisplay();
+	libesp::GUI &getGUI();
+	MenuState *getMenuState();
+	CalibrationMenu *getCalibrationMenu();
+	libesp::DisplayMessageState *getDisplayMessageState(libesp::BaseMenu *, const char *msg, uint32_t msDisplay);
+	libesp::XPT2046 &getTouch();
+	uint8_t *getBackBuffer();
+	uint32_t getBackBufferSize();
+protected:
+	MyApp();
+	virtual libesp::ErrorType onInit();
+	virtual libesp::ErrorType onRun();
+private:
+	MyErrorMap AppErrors;
+  MODE CurrentMode;
+  uint32_t LastTime;
+private:
+	static MyApp mSelf;
+};
+
+#endif /* DC27_APP_H */
