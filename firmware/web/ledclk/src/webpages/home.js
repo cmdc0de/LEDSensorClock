e9e29bd9189796b3d1650745ed37cc87ed704a81
cmdc0de <cmdc0dez@gmail.com>
2022-02-14 22:47:42 -0700

    test for scan appears to work

--- firmware/web/ledclk/src/webpages/home.js
+++ firmware/web/ledclk/src/webpages/home.js
@@ -3,10 +3,9 @@ import React from 'react';
 const Home = () => {
   return(
         <div>
-            <h1>home</h1>
+            <h1><a href="/scan"> Scan</a></h1>
         </div>
     );
 }
 
 export default Home;
-
--- firmware/web/ledclk/src/webpages/scan.js
+++ firmware/web/ledclk/src/webpages/scan.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import "../App.css"
 
-const WiFiScan = () => {
-  return(
-        <div>
-            <h1>Scan Lists</h1>
-        </div>
-    );
+const Home = () => {
+  const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [aps, setAps] = useState([]);    
+
+  useEffect(() => {
+        fetch("https://my-json-server.typicode.com/cmdc0de/LEDSensorClock/scanresults")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setAps(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+
+  if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+      return (
+        <table class="tg">
+          <thead>
+              <tr>
+                <td class="tg-e6ik"> id</td>
+                <td class="tg-e6ik"> ssid</td>
+                <td class="tg-e6ik"> strength</td>
+                <td class="tg-e6ik"> channel</td>
+                <td class="tg-e6ik"> AuthMode</td>
+              </tr>
+          </thead>
+            {aps.map(ap => (
+              <tr>
+                <td class="tg-e6ik"> {ap.id}</td>
+                <td class="tg-e6ik"> {ap.ssid}</td>
+                <td class="tg-e6ik"> {ap.rssi}</td>
+                <td class="tg-e6ik"> {ap.channel}</td>
+                <td class="tg-e6ik"> {ap.authMode}</td>
+              </tr>
+                ))}
+          </table>
+        );
+    }
 }
-export default WiFiScan;
+
+export default Home;
+
